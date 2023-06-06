0f1747fb0e1c57a493f2550ead1c374f89a0737e
Kekomister <jeremyg1999@example.com>
2023-06-06 13:35:16 -0300

    Arreglo de codigo productos

--- src/app/page/producto/producto.component.ts
+++ src/app/page/producto/producto.component.ts
@@ -30,17 +30,17 @@ export class ProductoComponent implements OnInit {
   // variable que guarda todos los generos
   generos: string[] = [];
   // arreglo que voy a usar para funcion de filtro
-  juegoRecibido: any[] = [];
+  //juegoRecibido: any[] = [];
 
   recibirJuegos(js: Juego[]) {
     //console.log(this.router.navigated);
     this.juegos = js;
     for (let i = 0; i < this.juegos.length; i++) {
-      this.juegoRecibido.push(this.juegos)
+      //this.juegoRecibido.push(this.juegos)
       this.chequearJuego(this.juegos[i]);
     }
     if (this.textoBuscar != "") {
-      this.buscar();
+      this.llenarJuegosElegidos("busqueda", this.textoBuscar);
     }
   }
 
@@ -112,41 +112,95 @@ export class ProductoComponent implements OnInit {
     }
   }
 
-  consolaElegida(csla: string) {
-    this.elegidoTrue();
+  llenarJuegosElegidos(caso: string, opcion: any) {
+
+    // esto hace que la pagina se vea para casos especificos (consola,año,etc)
     this.elegido = true;
     // vacio la variable antes de usarla para que si ya la habia usado, no tenga juegos de antemano
     this.jElegidos = [];
-    // entro en el array de juegos global
-    for (let i = 0; i < this.juegos.length; i++) {
-      // entro en el array de consolas del juego en el que estoy ahora mismo
-      for (let j = 0; j < this.juegos[i].consolas.length; j++) {
-        // si ese juego, en la consola que estoy ahora, es la que busco, lo agrego a los ELEGIDOS
-        if (this.juegos[i].consolas[j] == csla) {
-          this.jElegidos.push(this.juegos[i]);
+
+    caso = caso.toLocaleLowerCase();
+
+    switch (caso) {
+
+      case "genero":
+        // entro en el array de juegos global
+        for (let i = 0; i < this.juegos.length; i++) {
+          // entro en el array de generos del juego en el que estoy ahora mismo
+          for (let j = 0; j < this.juegos[i].genero.length; j++) {
+            // si ese juego que estoy ahora, tiene el genero que busco, lo agrego a los ELEGIDOS
+            if (this.juegos[i].genero[j] == opcion) {
+              this.jElegidos.push(this.juegos[i]);
+            }
+          }
         }
-      }
-    }
-  }
+        break;
 
-  precioElegido(pcio: string) {
-    // esto hace que la pagina se vea para casos especificos (consola,año,etc)
-    this.elegidoTrue();
-    // vacio la variable antes de usarla para que si ya la habia usado, no tenga juegos de antemano
-    this.jElegidos = [];
+      case "consola":
+        // entro en el array de juegos global
+        for (let i = 0; i < this.juegos.length; i++) {
+          // entro en el array de consolas del juego en el que estoy ahora mismo
+          for (let j = 0; j < this.juegos[i].consolas.length; j++) {
+            // si ese juego, en la consola que estoy ahora, es la que busco, lo agrego a los ELEGIDOS
+            if (this.juegos[i].consolas[j] == opcion) {
+              this.jElegidos.push(this.juegos[i]);
+            }
+          }
+        }
+        break;
 
-    // el precio que estoy buscando es de [*** a ***], entonces necesito separarlo en inicio y fin
-    let inicio = this.rangoInicio(pcio);
-    let fin = this.rangoFin(pcio);
+      case "precio":
 
-    // entro en el array de juegos global
-    for (let i = 0; i < this.juegos.length; i++) {
-      // paso mi precio a numero (no me acuerdo porque lo hize texto, pero lo necesitaba)
-      let precio = Number(this.juegos[i].precio);
-      // si ese juego que estoy ahora, tiene un precio dentro de lo que busco, lo agrego a los ELEGIDOS
-      if (precio >= inicio && precio <= fin) {
-        this.jElegidos.push(this.juegos[i]);
-      }
+        // el precio que estoy buscando es de [*** a ***], entonces necesito separarlo en inicio y fin
+        let inicio = this.rangoInicio(opcion);
+        let fin = this.rangoFin(opcion);
+
+        // entro en el array de juegos global
+        for (let i = 0; i < this.juegos.length; i++) {
+          // paso mi precio a numero (no me acuerdo porque lo hize texto, pero lo necesitaba)
+          let precio = Number(this.juegos[i].precio);
+          // si ese juego que estoy ahora, tiene un precio dentro de lo que busco, lo agrego a los ELEGIDOS
+          if (precio >= inicio && precio <= fin) {
+            this.jElegidos.push(this.juegos[i]);
+          }
+        }
+        break;
+
+      case "anio":
+
+        // entro en el array de juegos global
+        for (let i = 0; i < this.juegos.length; i++) {
+          // si ese juego que estoy ahora, tiene el año que busco, lo agrego a los ELEGIDOS
+          if (this.juegos[i].anioRelease == opcion) {
+            this.jElegidos.push(this.juegos[i]);
+          }
+        }
+        break;
+
+      case "requisito":
+
+        // entro en el array de juegos global
+        for (let i = 0; i < this.juegos.length; i++) {
+          // si ese juego que estoy ahora, tiene los requisitos que busco, lo agrego a los ELEGIDOS
+          if (this.juegos[i].requisitos?.toLocaleLowerCase() == opcion) {
+            this.jElegidos.push(this.juegos[i]);
+          }
+        }
+        break;
+
+      case "busqueda":
+        for (let i = 0; i < this.juegos.length; i++) {
+          // busco los juegos que dentro de su nombre tengan el texto de la variable 'value'
+          // los paso a lowercase para que no haya problemas de mayusculas
+          if (this.juegos[i].nombre?.toLowerCase().includes(opcion.toLowerCase())) {
+            this.jElegidos.push(this.juegos[i]);
+          }
+        }
+        break;
+
+      default:
+        alert("NO DEBERIA ENTRAR AQUI");
+        break;
     }
   }
 
@@ -178,41 +232,6 @@ export class ProductoComponent implements OnInit {
     return n;
   }
 
-  anioElegido(anio: number) {
-    // esto hace que la pagina se vea para casos especificos (consola,año,etc)
-    this.elegidoTrue();
-    // vacio la variable antes de usarla para que si ya la habia usado, no tenga juegos de antemano
-    this.jElegidos = [];
-    // entro en el array de juegos global
-    for (let i = 0; i < this.juegos.length; i++) {
-      // si ese juego que estoy ahora, tiene el año que busco, lo agrego a los ELEGIDOS
-      if (this.juegos[i].anioRelease == anio) {
-        this.jElegidos.push(this.juegos[i]);
-      }
-    }
-  }
-
-  generoElegido(gen: string) {
-    // esto hace que la pagina se vea para casos especificos (consola,año,etc)
-    this.elegidoTrue();
-    // vacio la variable antes de usarla para que si ya la habia usado, no tenga juegos de antemano
-    this.jElegidos = [];
-    // entro en el array de juegos global
-    for (let i = 0; i < this.juegos.length; i++) {
-      // entro en el array de generos del juego en el que estoy ahora mismo
-      for (let j = 0; j < this.juegos[i].genero.length; j++) {
-        // si ese juego que estoy ahora, tiene el genero que busco, lo agrego a los ELEGIDOS
-        if (this.juegos[i].genero[j] == gen) {
-          this.jElegidos.push(this.juegos[i]);
-        }
-      }
-    }
-  }
-
-  elegidoTrue() {
-    this.elegido = true;
-  }
-
   // funcion para poner los productos en su manera natural
   reset() {
     this.jElegidos = [];
@@ -220,45 +239,31 @@ export class ProductoComponent implements OnInit {
     this.router.navigate(['/producto']);
   }
 
-  buscar() {
-    // esto hace que la pagina se vea para casos especificos (consola,año,etc)
-    this.elegidoTrue();
-    // vacio la variable antes de usarla para que si ya la habia usado, no tenga juegos de antemano
-    this.jElegidos = [];
-    // entro en el array de juegos global
-    for (let i = 0; i < this.juegos.length; i++) {
-      // busco los juegos que dentro de su nombre tengan el texto de la variable 'value'
-      // los paso a lowercase para que no haya problemas de mayusculas
-      if (this.juegos[i].nombre?.toLowerCase().includes(this.textoBuscar.toLowerCase())) {
-        this.jElegidos.push(this.juegos[i]);
-      }
-    }
-
-  }
 
   textoEnBuscar() {
-    let aux = this.textoBuscar;
     this.route.paramMap.subscribe((params) => {
       const id = params.get('value');
       if (id != null) {
         this.textoBuscar = String(id);
-        this.buscar();
+        this.llenarJuegosElegidos("busqueda", this.textoBuscar);
       }
     });
   }
 
-  filtrarPorRequisito(requi:string):void{
-    console.log(requi);
-    console.log(this.juegoRecibido);
-    
-    this.elegidoTrue();
-    this.jElegidos = []
-    this.juegoRecibido.map((juego , index)=> {
-      juego[index].requisitos.toLowerCase() === requi.toLowerCase() ? this.jElegidos.push(juego[index]) : null;
-    })
-    console.log(this.jElegidos);
-    // this.jElegidos.push(juego[index])
-  }
+  // filtrarPorRequisito(requi: string): void {
+  //   console.log(requi);
+  //   console.log(this.juegoRecibido);
+
+  //   this.elegido = true;
+  //   this.jElegidos = []
+  //   this.llenarJuegosElegidos("requisito", requi);
+  //   this.juegoRecibido.map((juego, index) => {
+  //     juego[index].requisitos.toLowerCase() === requi.toLowerCase() ? this.jElegidos.push(juego[index]) : null;
+  //   })
+  //   console.log(this.jElegidos);
+  //   // this.jElegidos.push(juego[index])
+  // }
+
   constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
