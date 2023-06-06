6019bacaad0bedc597570475f90a7cd781cf5f2d
Gabriel Magallan <gabriel.magallan@outlook.es>
2023-06-06 17:31:17 -0300

    service gabo

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -10,7 +10,7 @@ import { User } from 'src/app/models/user.models';
 export class HeaderComponent {
   verModal: boolean = false
   estadLog: boolean = false
-  textoBuscar : string = "";
+  textoBuscar : string = " ";
   userPrueba: User = new User ("usuario" , "usuario@gmail.com" , "usuario")
   
   capturarContrasenia: string | undefined;
